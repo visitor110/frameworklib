a05664fd48a575cb803d886fe9ec70702de049d1
裴桐 <1187038691@qq.com>
2022-08-23 11:00:38 +0800

    初始化

---
+++ src/main/java/com/pt/javalib/JavalibApplication.java
@@ -0,0 +1,13 @@
+package com.pt.javalib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavalibApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavalibApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pt/javalib/JavalibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pt.javalib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavalibApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
